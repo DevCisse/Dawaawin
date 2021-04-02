cbd0d6094dcf7a7903ec09df404f1793b55ac104
Salihu Hassan <hassan3130@outlook.com>
2021-04-02 10:07:36 +0100

    Add clasesea and some json files

---
+++ Models/All.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dawaawin.Models
+{
+    public class All
+    {
+        public Fasl Fasl { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
